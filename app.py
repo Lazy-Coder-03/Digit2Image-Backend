a09b5297f4b3f4fa8b6c34cf8b7593641150dd7a
Sayantan Ghosh <ghosh2003sayantan@gmail.com>
2024-10-16 17:37:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,6 +65,6 @@ async def root():
     return {"message": "Hello, World!"}
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
